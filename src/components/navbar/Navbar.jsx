ae43d1246e42c5885652341b51fbcace5de83301
Talha Muhammad <121675123+talha1230@users.noreply.github.com>
2024-02-20 16:12:00 +0800

    Add responsive menu functionality to Navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from "react";
-//import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import React, { useState } from "react";
 import logo from 'C:/Users/Talha PC/chatai/src/assets/logo.svg';
 import './navbar.css';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <div className="chatai_navbar">
             <div className="chatai_navbar-links">
@@ -12,17 +14,20 @@ const Navbar = () => {
                         <img src={logo} alt="Chat AI" />
                     </a>
                 </div>
-                <div className="chatai_navbar-link-container">
+                <div className={`chatai_navbar-link-container ${isOpen ? 'open' : ''}`}>
                     <p> <a href="#home">Home</a></p>
                     <p> <a href="#features">Features</a></p>
                     <p> <a href="#possibility">Chat AI</a></p>
                     <p> <a href="#blog">Blog</a></p>
                     <p> <a href="#whatChatai">What is Chatai</a></p>
-                </div>
-                <div className="chatai_navbar-sign">
-                <p>Sign in</p>
-                <button type="button">Sign Up</button>
+                    <div className="chatai_navbar-sign">
+                        <p>Sign in</p>
+                        <button type="button">Sign Up</button>
                     </div>
+                </div>
+                <div className="menu-icon" onClick={() => setIsOpen(!isOpen)}>
+                    {isOpen ? <RiCloseLine /> : <RiMenu3Line />}
+                </div>
             </div>
         </div>
     );      
