8f4ceab37f7f05363d3746faa4e4d2d4aee9721b
Talha Muhammad <121675123+talha1230@users.noreply.github.com>
2024-02-26 03:38:05 +0800

    Remove unnecessary import statement in Article.jsx

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './article.css';
-
 const Article = ({ imgUrl, date, text }) => (
   <div className="gpt3__blog-container_article">
     <div className="gpt3__blog-container_article-image">
