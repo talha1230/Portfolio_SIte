bc1a6b40cc2ebe6cae03c44e9a3e23ed087ed43f
Talha Muhammad <121675123+talha1230@users.noreply.github.com>
2024-02-28 23:39:26 +0800

    Update package-lock.json and package.json, add react-visibility-sensor to dependencies, and modify Possibility component to include a button for exploring projects

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import VisibilitySensor from 'react-visibility-sensor';
 import possibilityImage from '../../assets/possibility.png';
 import './possibility.css';
 
-const Possibility = () => (
-  <div className="gpt3__possibility section__padding" id="possibility">
+const Possibility = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <VisibilitySensor
+      partialVisibility
+      onChange={(isVisible) => setIsVisible(isVisible)}
+    >
+      <div style={{ opacity: isVisible ? 1 : 0 }}>
+        <div className="gpt3__possibility section__padding" id="possibility">
     <div className="gpt3__possibility-image">
       <img src={possibilityImage} alt="possibility" />
     </div>
     <div className="gpt3__possibility-content">
-      <h4>Request Early Access to Get Started</h4>
+      <button type="button" onClick={() => window.open("https://talhakhan.me/", "_blank")}>
+        <svg
+          height="24"
+          width="24"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M0 0h24v24H0z" fill="none"></path>
+          <path
+            d="M5 13c0-5.088 2.903-9.436 7-11.182C16.097 3.564 19 7.912 19 13c0 .823-.076 1.626-.22 2.403l1.94 1.832a.5.5 0 0 1 .095.603l-2.495 4.575a.5.5 0 0 1-.793.114l-2.234-2.234a1 1 0 0 0-.707-.293H9.414a1 1 0 0 0-.707.293l-2.234 2.234a.5.5 0 0 1-.793-.114l-2.495-4.575a.5.5 0 0 1 .095-.603l1.94-1.832C5.077 14.626 5 13.823 5 13zm1.476 6.696l.817-.817A3 3 0 0 1 9.414 18h5.172a3 3 0 0 1 2.121.879l.817.817.982-1.8-1.1-1.04a2 2 0 0 1-.593-1.82c.124-.664.187-1.345.187-2.036 0-3.87-1.995-7.3-5-8.96C8.995 5.7 7 9.13 7 13c0 .691.063 1.372.187 2.037a2 2 0 0 1-.593 1.82l-1.1 1.039.982 1.8zM12 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
+            fill="currentColor"
+          ></path>
+        </svg>
+        <span>Explore My Projects</span>
+      </button>
       <h1 className="gradient__text">The possibilities are <br /> beyond your imagination</h1>
-      <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      <p>Yet bed any for traveling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
       <h4>Request Early Access to Get Started</h4>
     </div>
-  </div>
-);
+    </div>
+      </div>
+    </VisibilitySensor>
+  );
+};
 
 export default Possibility;
