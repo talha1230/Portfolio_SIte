69c3f18ae6514cd352d2ceed928e233b1b7ba633
Talha Muhammad <121675123+talha1230@users.noreply.github.com>
2024-03-01 00:21:41 +0800

    Update footer button text

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => (
     </div>
 
     <div className="gpt3__footer-btn">
-      <p>Request Early Access</p>
+      <p>Lets Collaborate</p>
     </div>
 
     <div className="gpt3__footer-links"></div>
