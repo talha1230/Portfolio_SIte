696b8ab8b98e9db0433547185aa0e0cfa93c24dc
Talha Muhammad <121675123+talha1230@users.noreply.github.com>
2024-03-02 18:47:17 +0800

    Update feature.css and Footer.jsx

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -2,39 +2,50 @@ import React from 'react';
 import gpt3Logo from '../src/assets/cat.svg';
 import './footer.css';
 
-const Footer = () => (
-  <div className="gpt3__footer section__padding">
-    <div className="gpt3__footer-heading">
-      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
-    </div>
+const Footer = () => {
+  const [clickCount, setClickCount] = React.useState(0);
 
-    <div className="gpt3__footer-btn">
-      <p>Lets Collaborate</p>
-    </div>
+  const handleClick = () => {
+    setClickCount(clickCount + 1);
+  };
 
-    <div className="gpt3__footer-links"></div>
-    <div className="gpt3__footer-links_logo">
-      <img src={gpt3Logo} alt="gpt3_logo" />
-      <p>Your Name, <br /> All Rights Reserved</p>
+  return (
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">Do you want to step into the future before others</h1>
+      </div>
+
+      <div className="gpt3__footer-btn">
+        <button onClick={handleClick} className="button" data-text="Awesome">
+          <span className="actual-text">&nbsp;Lets Collaborate&nbsp;</span>
+          <a href="https://github.com/talha1230" target="_blank" rel="noopener noreferrer" className="hover-text">&nbsp;Github&nbsp;</a>
+        </button>
+      </div>
+
+      <div className="gpt3__footer-links"></div>
+      <div className="gpt3__footer-links_logo">
+        <img src={gpt3Logo} alt="gpt3_logo" />
+        <p>Your Name, <br /> All Rights Reserved</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Quick Links</h4>
+        <p>About Me</p>
+        <p>Skills</p>
+        <p>Projects</p>
+        <p>Education</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>More</h4>
+        <p>Blog</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Contact Info</h4>
+        <p>PJ, KL, Malaysia</p>
+        <p>b09230027@student.unimy.edu.my</p>
+      </div>
     </div>
-    <div className="gpt3__footer-links_div">
-      <h4>Quick Links</h4>
-      <p>About Me</p>
-      <p>Skills</p>
-      <p>Projects</p>
-      <p>Education</p>
-    </div>
-    <div className="gpt3__footer-links_div">
-      <h4>More</h4>
-      <p>Blog</p>
-      <p>Contact</p>
-    </div>
-    <div className="gpt3__footer-links_div">
-      <h4>Contact Info</h4>
-      <p>PJ, KL, Malaysia</p>
-      <p>b09230027@student,unimy,edu.my.com</p>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Footer;
